4cfccb7c66d0ebe5c6248d21e2137f8a261f2ad2
Andrew Cer <cer.andrew@gmail.com>
2015-09-04 09:57:03 -0600

    server and client side auth for signup up and running

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -1,7 +1,65 @@
-app.controller('AccountController', ['$scope', function ($scope) {
-  // $scope.signUp = function () {
-  //   $scope.showForm = true;
-  // }
+app.controller('AccountController', ['$scope', '$http', function ($scope, $http) {
+  $scope.signUp = function () {
+    $scope.nameError = null;
+    $scope.nameShortError = null;
+    $scope.nameLongError = null;
+    $scope.nameExists = null;
+    $scope.emailExists = null;
+    $scope.passwordError = null;
+    $scope.passwordMatchError = null;
+    $scope.serverError = null;
+    var username = $scope.userName;
+    var email = $scope.email;
+    var password = $scope.password;
+    var passCheck = $scope.passCheck;
+    var validationCheck = function (name, password, passwordCheck) {
+      var illegals = /\W/;
+      var errorArray = [];
+      if (name.length < 5) {
+        $scope.nameShortError = 'Username is too short. Must be longer than 4 characters';
+        errorArray.push(true);
+      }
+      if (name.length > 15) {
+        $scope.nameLongError = 'Username is too long. Must be less than 15 characters';
+        errorArray.push(true);
+      }
+      if (illegals.test(name)) {
+        $scope.nameError = 'Username contains illegal characters. Can only use letters, numbers and underscores (no spaces)';
+        errorArray.push(true);
+      }
+      if (password.length < 6) {
+        $scope.passwordError = 'Password is too short. Must be greater than 5 characters';
+        errorArray.push(true);
+      }
+      if (password != passwordCheck) {
+        $scope.passwordMatchError = 'Passwords do not match';
+        errorArray.push(true);
+      }
+      return errorArray;
+    }
+    if (validationCheck(username, password, passCheck).length === 0) {
+      $http.post('api/check-input', { username: username, email: email})
+        .success(function (response) {
+          if (response.username === true) {
+            $scope.nameExists = 'That user name is taken';
+          }
+          if (response.email === true) {
+            $scope.emailExists = 'That Email is taken';
+          }
+          else {
+            $http.post('api/authenticate', { username: username, email: email, password: password, passCheck: passCheck})
+              .success(function (response) {
+                if (response === true) {
+
+                }
+                else {
+                  $scope.serverError = 'Something went wrong. Please try again'
+                }
+            });
+          }
+        })
+    }
+  }
 }])
 
 app.controller('HomeController', ['$scope', '$http', 'Warrior', 'Shaman', 'Rogue',
--- routes/api.js
+++ routes/api.js
@@ -3,14 +3,61 @@ var router = express.Router();
 var unirest = require('unirest');
 var db = require('monk')('localhost/hearthnode');
 var cards = db.get('cards');
+var users = db.get('users');
+var validateSignUp = require('../lib/validations.js').validation;
 
-router.get('/deck/:class', function(req, res, next) {
+router.get('/deck/:class', function(req, res) {
   var className = req.params.class;
   cards.findOne({class: className})
   .then(function (data) {
     res.json(data)
-  })
+})
 
+router.post('/check-input', function (req, res) {
+  var username = req.body.username;
+  var email = req.body.email;
+  console.log(email);
+  var returnObj = {}
+  users.findOne({username: username})
+  .then(function (user) {
+    if (user) {
+      return returnObj.username = true;
+    }
+    else {
+      return returnObj.username = false;
+    }
+  })
+  .then(function () {
+    return users.findOne({email: email})
+    .then(function (user) {
+      if (user) {
+        return returnObj.email = true;
+      }
+      else {
+        return returnObj.email = false;
+      }
+    })
+  })
+  .then(function () {
+    return res.json(returnObj)
+  })
+})
+
+router.post('/authenticate', function (req, res) {
+  var username = req.body.username;
+  var email = req.body.email;
+  var password = req.body.password;
+  var passCheck = req.body.passCheck;
+  if (validateSignUp(username, password, passCheck).length === 0) {
+    users.insert(req.body)
+    .then(function () {
+      res.json(true);
+    })
+  }
+  else {
+    res.json(false);
+  }
+})
 
   //for database seeed
   // unirest.get("https://omgvamp-hearthstone-v1.p.mashape.com/cards/classes/" + req.params.class)
