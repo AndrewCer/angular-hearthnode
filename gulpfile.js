41c331b5c11ef17e70871315b0e4ca2bf5123dd5
Andrew Cer <cer.andrew@gmail.com>
2015-09-10 13:35:07 -0600

    gulp minifcation and concat changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
 gulp.task('scripts', function () {
   return gulp.src('public/javascripts/*js')
   .pipe(concat('all.js'))
+  .pipe(uglify())
   .pipe(gulp.dest('public/dist'))
 })
 
